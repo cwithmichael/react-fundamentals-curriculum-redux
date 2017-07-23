96ee4e044160e67a6f891f8c82b3d0912cf40114
Michael <cwithmichael@gmail.com>
2017-07-23 03:26:05 -0500

    fixing typo

--- app/containers/App.js
+++ app/containers/App.js
@@ -7,7 +7,7 @@ import {
 import Home from '../components/Home'
 import Header from '../components/Header'
 
-class App extends component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
