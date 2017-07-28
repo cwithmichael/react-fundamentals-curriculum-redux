54291fba79247342a89cc35a895e30ca1179df17
Michael Terrell <cwithmichael@gmail.com>
2017-07-28 16:49:09 -0500

    making the title in the header clickable so that it takes the user to the / route

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link }  from 'react-router-dom';
 import PropTypes from 'prop-types';
 import CityForm from '../components/CityForm';
 
@@ -18,7 +19,9 @@ const Header = (props) => {
       <div id="header" className="container-fluid" style={{backgroundColor:'orange'}}>
         <div className="row-fluid">
           <div className="col-md-4">
-            <h2 style={{paddingTop: 0, color: 'white'}}>Weather Forecast</h2>
+            <Link to='/' style={{textDecoration:'none'}}>
+              <h2 style={{paddingTop: 0, color: 'white'}}>Weather Forecast</h2>
+            </Link>
           </div>
           <div className="col-md-offset-8">
             <CityForm parentName='header' style={divStyle} {...props}/>
